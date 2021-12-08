8b8703291736f3bec2affc47a4c9e557579f9eae
Abhay Goel <abhaygoel1906@gmail.com>
2021-12-08 18:33:22 -0500

    Add files via upload

--- mavenproject3/src/main/java/Test2Neo4j.java
+++ mavenproject3/src/main/java/Test2Neo4j.java
@@ -12,7 +12,22 @@ import java.util.*;
 import static org.neo4j.driver.Values.parameters;
 
 class Thr extends Thread {
+    Transaction tx;
+    String s;
     
+    public Thr(Transaction tx, String s) {
+        this .tx = tx;
+        this.s = s;
+    }
+    public void run() {
+        try{
+            //long l1 = System.nanoTime();
+            Result rs = tx.run(s);
+            //long l2 = System.nanoTime();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
 public class Test2Neo4j {
@@ -20,6 +35,16 @@ public class Test2Neo4j {
     
     public static void main(String[] args) {
         String srr[] = new String[10];
+        srr[0] = "MATCH (t:Tweet) where t.text contains 'http' and t.likes > 100 return t;";
+        srr[1] = "Match (u:User)-[:AT]->(g:Geo) where g.locn contains 'U.S' and not(u.bio is null) and not(u.dp is null) return u;";
+        srr[2] = "match (t:Tweet)-[:MENTIONED]->(u:User) return t;";
+        srr[3] = "MATCH (u:User)-[:Tweeted]->(t:Tweet) where t.likes < 30 and u.follower > 250 return t.tid;";
+        srr[4] = "match (t:Tweet) where t.retweets > t.likes return t;";
+        srr[5] = "match (u:User) where u.verified = true and u.follower > 1000 return u;";
+        srr[6] = "MATCH (g:Geo)<-[:AT]-(u:User)-[:Tweeted]->(t:Tweet) where not( g.locn contains 'US') return t;";
+        srr[7] = "MATCH (n)-[:FOLLOWS]->(m) where n.verified = true and m.verified = false return m;";
+        srr[8] = "MATCH (n:Tweet) where n.year > 2015 and n.year <2021 return n;";
+        srr[9] = "MATCH (n:User) where n.username contains 'A' or n.username contains 'a' return n;";
         
         try{
             String uri = "bolt://localhost:7687", u = "neo4j", p = "qwert123";
@@ -33,67 +58,15 @@ public class Test2Neo4j {
             long l1 = System.nanoTime();
             Result rs = tx.run(q);
             long l2 = System.nanoTime();
-            System.out.println("Query 1: " + (l2-l1)/1000000);
-            
-            q="MATCH (t:Tweet) where t.text contains 'http' and t.likes > 100 return t;";
-            long l11 = System.nanoTime();
-            rs = tx.run(q);
-            long l12 = System.nanoTime();
-            System.out.println("Query 1: " + (l12-l11)/1000000);
-            
-            q = "Match (u:User)-[:AT]->(g:Geo) where g.locn contains 'U.S' and not(u.bio is null) and not(u.dp is null) return u;";
-            long l21 = System.nanoTime();
-            rs = tx.run(q);
-            long l22 = System.nanoTime();
-            System.out.println("Query 2: " + (l22-l21)/1000000);
-            
-            q = "match (t:Tweet)-[:MENTIONED]->(u:User) return t;";
-            long l31 = System.nanoTime();
-            rs = tx.run(q);
-            long l32 = System.nanoTime();
-            System.out.println("Query 3: " + (l32-l31)/1000000);
-            
-            q = "MATCH (u:User)-[:Tweeted]->(t:Tweet) where t.likes < 30 and u.follower > 250 return t.tid;";
-            long l41 = System.nanoTime();
-            rs = tx.run(q);
-            long l42 = System.nanoTime();
-            System.out.println("Query 4: " + (l42-l41)/1000000);
-            
-            q = "match (t:Tweet) where t.retweets > t.likes return t;";
-            long l51 = System.nanoTime();
-            rs = tx.run(q);
-            long l52 = System.nanoTime();
-            System.out.println("Query 5: " + (l52-l51)/1000000);
-            
-            q = "match (u:User) where u.verified = true and u.follower > 1000 return u;";
-            long l61 = System.nanoTime();
-            rs = tx.run(q);
-            long l62 = System.nanoTime();
-            System.out.println("Query 6: " + (l62-l61)/1000000);
-            
-            q = "MATCH (g:Geo)<-[:AT]-(u:User)-[:Tweeted]->(t:Tweet) where not( g.locn contains 'US') return t;";
-            long l71 = System.nanoTime();
-            rs = tx.run(q);
-            long l72 = System.nanoTime();
-            System.out.println("Query 7: " + (l72-l71)/1000000);
-            
-            q = "MATCH (n)-[:FOLLOWS]->(m) where n.verified = true and m.verified = false return m;";
-            long l81 = System.nanoTime();
-            rs = tx.run(q);
-            long l82 = System.nanoTime();
-            System.out.println("Query 8: " + (l82-l81)/1000000);
-            
-            q = "MATCH (n:Tweet) where n.year > 2015 and n.year <2021 return n;";
-            long l91 = System.nanoTime();
-            rs = tx.run(q);
-            long l92 = System.nanoTime();
-            System.out.println("Query 9: " + (l92-l91)/1000000);
-            
-            q = "MATCH (n:User) where n.username contains 'A' or n.username contains 'a' return n;";
-            long l01 = System.nanoTime();
-            rs = tx.run(q);
-            long l02 = System.nanoTime();
-            System.out.println("Query 10: " + (l02-101)/1000000);
+            //System.out.println("Query 1: " + (l2-l1)/1000000);
+            for(int i=0; i<srr.length; i++) {
+                Thr temp = new Thr(tx, srr[i]);
+                l1 = System.nanoTime();
+            //Result rs = tx.run(s);
+                temp.start();
+                l2 = System.nanoTime();
+                System.out.println((i+1)+": " + (l2-l1));
+            }
             
             //System.out.println( g );            
         } catch (Exception e) {
